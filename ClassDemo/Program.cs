9d41ac40066caa44a77cee58e53fc5647fe30789
enbansa <39252271+enbansa@users.noreply.github.com>
2019-11-13 08:48:27 -0600

    Add files via upload

---
+++ ClassDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle vehicle = new Vehicle("Blue", 2);
+            Vehicle vehicle2 = new Vehicle("Red", 16);
+            Vehicle[] vehicles = new Vehicle[2] { vehicle, vehicle2 };
+            vehicle.Start();
+            vehicle2.Start();
+            vehicle2.DeAccelerate(-5);
+            vehicle.Accelerate(25);
+             foreach(var item in vehicles)
+            {
+                Console.WriteLine(item.GetVehicleInfo());
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ ClassDemo/Vehicle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{
+    class Vehicle
+    {
+        private string Color { get; set; }
+
+        private int Speed { get; set; }
+
+        private int WheelCount { get; set; }
+
+        //Constructor
+
+        public Vehicle(string color,int wheelCount)
+        {
+            Color = color;
+            WheelCount = wheelCount;
+        }
+
+        public string GetVehicleInfo()
+        {
+            var info = string.Format("Color : {0},Speed {1}," + " WheelCount : {2} ", Color, Speed, WheelCount); 
+            return info;
+        }
+
+        public void Start()
+        {
+            Speed = 20;
+        }
+
+        public void Stop()
+        {
+            Speed = 0;
+        }
+
+        public void Accelerate(int value)
+        {
+            Speed += value;
+        }
+
+        public void DeAccelerate(int value)
+        {
+            Speed = Speed - value;
+        }
+    }
+}
