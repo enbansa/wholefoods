49ed77ba3471b4d80716db4d31908f1cb50cfab2
enbansa <39252271+enbansa@users.noreply.github.com>
2019-11-13 08:52:25 -0600

    Add files via upload

---
+++ 5WhileLoop/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please enter your name");
+                var name = Console.ReadLine();
+                Console.WriteLine("Hello " + name + " Welcome to my name");
+                Console.WriteLine( "Try Again? If yes enter y");
+                var response = Console.ReadLine();
+                if (response != "y")
+                    break;
+
+
+
+
+
+            }
+            
+
+
+        }
+
+    }
+}
---
+++ AlertBox/Program.cs
@@ -0,0 +1,30 @@
+﻿using java.sql;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AlertBox
+{
+    class Program
+    {
+        static IWebDriver driver = new ChromeDriver();
+        static IAlert Alert;
+
+        static void Main(string[] args)
+        {
+            driver.Navigate().GoToUrl("http://testing.todvachev.com/category/special-elements/");
+            var box = driver.FindElement(By.XPath("//*[@id='main-content']/article[1]/div[2]/header/h3/a"));
+            box.Click();
+            Thread.Sleep(3000);
+            Alert = driver.SwitchTo().Alert();
+            Alert.Accept();
+            Thread.Sleep(5000);
+            driver.Quit();
+        }
+    }
+}
