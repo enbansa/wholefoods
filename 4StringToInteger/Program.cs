ea11badc5d9be50829035fecbef1d56f573b22fe
enbansa <39252271+enbansa@users.noreply.github.com>
2019-11-13 08:54:44 -0600

    Add files via upload

---
+++ 4StringToInteger/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4StringToInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your age");
+            //getting an input from user and storing it to string variable named name
+            string stringage = Console.ReadLine();
+            int age = Convert.ToInt32(stringage);
+            string response = "You will be " +( age+10) + " years old in 10 years";
+            Console.WriteLine(response);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 4forEachLoop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4forEachLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = new int[3] { 10, 20, 20 };
+            foreach (int eachInt in intArray)
+            {
+                Console.WriteLine(eachInt);
+            }
+
+            string[] stringArray = new string[3] { "one", "two", "three" };
+            foreach (string eachString in stringArray)
+            {
+                Console.WriteLine(eachString);
+            }
+        }  
+    }
+}
